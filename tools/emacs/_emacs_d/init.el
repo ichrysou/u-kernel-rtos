53dbf87feec203172b613718b7d073831d25b96e
ichrysou <ichrysou@gmail.com>
2016-12-21 10:09:02 +0100

    updated emacs configuration

--- tools/emacs/_emacs_d/init.el
+++ tools/emacs/_emacs_d/init.el
@@ -14,22 +14,152 @@
 
 (add-to-list 'load-path "~/.emacs.d/custom")
 (require 'sr-speedbar)
-(global-set-key [3 67108923] (quote sr-speedbar-open))
+(global-set-key [3 59] (quote sr-speedbar-toggle))
+(global-set-key [3 111] (quote sr-speedbar-select-window))
 (global-set-key [3 115] (quote sr-speedbar-select-window))
+
+(global-set-key [27 10] (quote org-insert-heading))
+
 (require 'auto-complete)
 (require 'auto-complete-config)
 (ac-config-default)
 (require 'yasnippet)
 (yas-global-mode 1)
-(setq c-default-style "k&r")
+;;(setq c-default-style "k&r")
+(setq c-default-style "linux")
+(setq c-basic-offset 4)
+(load-file "~/projects/emacs_stuff/rtags/rtags/build/src/rtags.el")
+;; ensure that we use only rtags checking
+;; https://github.com/Andersbakken/rtags#optional-1
+(global-flycheck-mode)
+(defun setup-flycheck-rtags ()
+  (interactive)
+  (flycheck-select-checker 'rtags)
+  ;; RTags creates more accurate overlays.
+  (setq-local flycheck-highlighting-mode nil)
+  (setq-local flycheck-check-syntax-automatically nil))
 
+
+
+;; rtags setup that has been working so far
+;; only run this if rtags is installed
+;; (when (require 'rtags nil :noerror)
+;;   ;; make sure you have company-mode installed
+;;   (require 'company)
+;;   (define-key c-mode-base-map (kbd "M-.")
+;;     (function rtags-find-symbol-at-point))
+;;   (define-key c-mode-base-map (kbd "M-,")
+;;     (function rtags-find-references-at-point))
+;;   (define-key c-mode-base-map (kbd "M-*")
+;;     (function rtags-location-stack-back))
+;;   ;; jump to definition and see the keybindings.
+;;   (rtags-enable-standard-keybindings))
+;;   ;; comment this out if you don't have or don't use helm
+;;   ;;(setq rtags-use-helm t)
+;;   ;; company completion setup
+
+;;try out rtags with fallback to gtags
+;;---------------------------------------------------------------------------------------------------
+(require 'company)
+
+(defun use-rtags (&optional useFileManager)
+  (and (rtags-executable-find "rc")
+       (cond ((not (gtags-get-rootpath)) t)
+             ((and (not (eq major-mode 'c++-mode))
+                   (not (eq major-mode 'c-mode))) (rtags-has-filemanager))
+             (useFileManager (rtags-has-filemanager))
+             (t (rtags-is-indexed)))))
+
+(defun tags-find-symbol-at-point (&optional prefix)
+  (interactive "P")
+  (if (and (not (rtags-find-symbol-at-point prefix)) rtags-last-request-not-indexed)
+      (gtags-find-tag)))
+(defun tags-find-references-at-point (&optional prefix)
+  (interactive "P")
+  (if (and (not (rtags-find-references-at-point prefix)) rtags-last-request-not-indexed)
+      (gtags-find-rtag)))
+(defun tags-find-symbol ()
+  (interactive)
+  (call-interactively (if (use-rtags) 'rtags-find-symbol 'gtags-find-symbol)))
+(defun tags-find-references ()
+  (interactive)
+  (call-interactively (if (use-rtags) 'rtags-find-references 'gtags-find-rtag)))
+(defun tags-find-file ()
+  (interactive)
+  (call-interactively (if (use-rtags t) 'rtags-find-file 'gtags-find-file)))
+(defun tags-imenu ()
+  (interactive)
+  (call-interactively (if (use-rtags t) 'rtags-imenu 'idomenu)))
+
+(define-key c-mode-base-map (kbd "M-.") (function tags-find-symbol-at-point))
+(define-key c-mode-base-map (kbd "M-,") (function tags-find-references-at-point))
+;;(define-key c-mode-base-map (kbd "M-;") (function tags-find-file))
+(define-key c-mode-base-map (kbd "C-.") (function tags-find-symbol))
+(define-key c-mode-base-map (kbd "C-,") (function tags-find-references))
+(define-key c-mode-base-map (kbd "C-<") (function rtags-find-virtuals-at-point))
+(define-key c-mode-base-map (kbd "M-i") (function tags-imenu))
+
+(define-key global-map (kbd "M-.") (function tags-find-symbol-at-point))
+(define-key global-map (kbd "M-,") (function tags-find-references-at-point))
+;;(define-key global-map (kbd "M-;") (function tags-find-file))
+(define-key global-map (kbd "C-.") (function tags-find-symbol))
+(define-key global-map (kbd "C-,") (function tags-find-references))
+(define-key global-map (kbd "C-<") (function rtags-find-virtuals-at-point))
+(define-key global-map (kbd "M-i") (function tags-imenu))
+(define-key c-mode-base-map (kbd "M-*")
+  (function rtags-location-stack-back))
+
+;;---------------------------------------------------------------------------------------------------
+;;--- try out irony mode ------
+(require 'company-irony-c-headers)
+;; Load with `irony-mode` as a grouped backend
+(eval-after-load 'company
+  '(add-to-list
+        'company-backends '(company-irony-c-headers company-irony)))
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
+
+;; replace the `completion-at-point' and `complete-symbol' bindings in
+;; irony-mode's buffers by irony-mode's function
+(defun my-irony-mode-hook ()
+  (define-key irony-mode-map [remap completion-at-point]
+    'company-irony)
+  (define-key irony-mode-map [remap complete-symbol]
+    'company-irony))
+(add-hook 'irony-mode-hook 'my-irony-mode-hook)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+;;---------------------------------------------------------------------------------------------------
+
+(global-set-key [27 112] (quote previous-error))
+(global-set-key [27 110] (quote next-error))
+(global-set-key [27 112] (quote rtags-previous-match))
+(global-set-key [27 110] (quote rtags-next-match))
+
+;;---------------------------------------------------------------------------------------------------
+;; Save all tempfiles in $TMPDIR/emacs$UID/
+(defconst emacs-tmp-dir (format "%s/%s%s/" temporary-file-directory "emacs" (user-uid)))
+(setq backup-directory-alist
+      `((".*" . ,emacs-tmp-dir)))
+(setq auto-save-file-name-transforms
+      `((".*" ,emacs-tmp-dir t)))
+(setq auto-save-list-file-prefix
+      emacs-tmp-dir)
+;;---------------------------------------------------------------------------------------------------
+
+(setq rtags-autostart-diagnostics t)
+(rtags-diagnostics)
+(setq rtags-completions-enabled t)
+(global-company-mode)
+(define-key c-mode-base-map (kbd "<C-tab>") (function company-complete))
+(setq make-backup-files nil)
 (require 'setup-general)
 (if (version< emacs-version "24.4")
     (require 'setup-ivy-counsel)
 ;;  (require 'setup-helm)
 ;;  (require 'setup-helm-gtags))
 )
-(require 'setup-ggtags)
+;;(require 'setup-ggtags)
 (require 'setup-cedet)
 (require 'setup-editing)
 (require 'setup-sr-speedbar)
